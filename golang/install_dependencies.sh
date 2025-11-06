97c62f9cc83341b0b35abb990de8e3bd35a05a4d
DevOps <devOps@berrybytes.com>
2025-11-06 13:32:58 +0545

    fix:asdf upgradation

--- golang/install_dependencies.sh
+++ golang/install_dependencies.sh
@@ -157,7 +157,7 @@ fi
 
 # List of mandatory tools and their versions
 declare -a mandatory_tools=(
-    # "gitleaks:8.21.0"
+    "gitleaks:8.21.0"
     # "yamlfmt:latest"
     "pre-commit:3.3.3"
     # "golangci-lint:1.63.4"
