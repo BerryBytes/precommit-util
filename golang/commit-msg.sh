70624cf39486e14ee9dad2093329de3f18e015a5
DevOps <devOps@berrybytes.com>
2025-11-10 10:30:26 +0545

    fix:asdf upgradation

--- golang/commit-msg.sh
+++ golang/commit-msg.sh
@@ -21,7 +21,7 @@ module.exports = {
         'type-empty': [2, 'never'],
         'scope-case': [2, 'always', 'lowerCase'],
         'subject-empty': [2, 'never'],
-        'subject-full-stop': [2, 'never', '.'],
+        'subject-full-stop': [2, 'never', '.'],  
         'header-max-length': [2, 'always', 72],   
     },
 };
